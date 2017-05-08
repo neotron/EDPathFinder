a2a2425da05782dd2d0eb85a270b84a8038b1774
David Hedbor <david@ookla.com>
2017-05-07 19:06:09 -0700

    Added additional ELW’s from the ELW list.

--- tools/coordinate-mapper/Sources/main.swift
+++ tools/coordinate-mapper/Sources/main.swift
@@ -10,19 +10,21 @@ struct System {
 }
 
 var valuableSystems = [Int:System]()
+var elws = try! String(contentsOfFile: "elws.txt", encoding: .isoLatin1).components(separatedBy: "\n")
+
+let elwsset = Set(elws.map { $0.uppercased() });
+elws.removeAll()
 
 func loadBodies() {
-    guard let systemrows = try? String(contentsOfFile: "valuable-bodies.jsonl").components(separatedBy: "\n") else {
-        exit(1)
-    }
+    let systemrows = try! String(contentsOfFile: "valuable-bodies.jsonl").components(separatedBy: "\n")
 
     var bodies = 0
-    var elw = 0
+    var elw = elwsset.count
     var ww = 0
     var tf = 0
     var aw = 0
-    var systems = 0
     for systemline in systemrows {
+    var systems = 0
         let systemjson = JSON(parseJSON: systemline)
         guard let systemid = systemjson["id"].int else {
             continue
@@ -35,20 +37,20 @@ func loadBodies() {
         var system = existingsystem ?? System()
         switch systemjson["type_name"] {
         case "Earth-like world":
-            system.elw += 1
+            system.elw = 1
             elw += 1
         case "Water world":
-            system.ww += 1
+            system.ww = 1
             ww += 1
             if systemjson["terraforming_state_id"] == 2 {
                 system.wwt += 1
             }
         case "Ammonia world":
-            system.aw += 1
+            system.aw = 1
             aw += 1
         default:
             if systemjson["terraforming_state_id"] == 2 {
-                system.tf += 1
+                system.tf = 1
                 tf += 1
             }
         }
@@ -58,7 +60,6 @@ func loadBodies() {
             print("\rBodies: \(bodies), ELW: \(elw), WW: \(ww), AW: \(aw), TF: \(tf)                 ", terminator: "")
             fflush(stdout)
         }
-
     }
     print("\rparsed \(bodies) bodies in \(valuableSystems.count) systems.")
 }
@@ -86,7 +87,7 @@ if let aStreamReader = StreamReader(path: "systems.csv") {
         guard let id = Int(row[0]), row[7] == "0" else {
             continue
         }
-
+        let systemName = row[2].replacingOccurrences(of: "\"", with: "")
 
         found += 1
         if (found % 1000) == 0 {
@@ -94,14 +95,19 @@ if let aStreamReader = StreamReader(path: "systems.csv") {
             fflush(stdout)
         }
 
-        guard let system = valuableSystems[id] else {
-            continue;
+        if let system = valuableSystems[id] {
+            let rowstring = "\(systemName)\t\(row[3])\t\(row[4])\t\(row[5])\t\(system.elw)\t\(system.ww)\t\(system.wwt)\t\(system.aw)\t\(system.tf)\n"
+            if let rowdata = rowstring.data(using: .isoLatin1, allowLossyConversion: false) {
+                handle.write(rowdata)
+            }
+        } else if(elwsset.contains(systemName.uppercased())) {
+            let rowstring = "\(systemName)\t\(row[3])\t\(row[4])\t\(row[5])\t1\t0\t0\t0\t0\n"
+            if let rowdata = rowstring.data(using: .isoLatin1, allowLossyConversion: false) {
+                handle.write(rowdata)
+            }
+
         }
-        let systemName = row[2].replacingOccurrences(of: "\"", with: "")
-        let rowstring = "\(systemName)\t\(row[3])\t\(row[4])\t\(row[5])\t\(system.elw)\t\(system.ww)\t\(system.wwt)\t\(system.aw)\t\(system.tf)\n"
-        if let rowdata = rowstring.data(using: .isoLatin1, allowLossyConversion: false) {
-            handle.write(rowdata)
-        }
+
     }
 }
 
