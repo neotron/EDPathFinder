d0ad2caa62ebc36d88b652a129a6e06e9fa1a453
David Hedbor <david@ookla.com>
2019-03-30 17:18:32 -0700

    Update valuable systems

--- tools/swift/Package.swift
+++ tools/swift/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
         name: "coordinate-mapper",
         dependencies: [
-            .package(url: "https://github.com/JohnSundell/Unbox", from: "2.3.0"),  
+            .package(url: "https://github.com/JohnSundell/Unbox", from: "3.0.0"),  
         ],
         targets: [
             .target(name: "coordinate-mapper"),
